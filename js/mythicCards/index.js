154c879cf14ebed6c45ab438407fc81aad61604c
EnayaAme <enaya13ame@gmail.com>
2022-08-25 17:13:19 +0200

    feat: without counter

---
+++ js/mythicCards/index.js
@@ -0,0 +1,11 @@
+import brownCards from './brown';
+import blueCards from './blue.index.js';
+import greenCards from './green';
+
+export {
+  brownCards,
+  blueCards,
+  greenCards
+}
+
+export default ancientsData
